d02aaf618e4ff706aa14139b0cf472160f224c8a
Niall Bennett <niallbennett01@yahoo.com>
2019-05-17 15:08:05 +0100

    reviews controller finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+  before_action :set_booking, only: [:new, :create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.booking = @booking
+    @review.save
+    redirect_to bookings_path
+  end
+
+  private
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
+
