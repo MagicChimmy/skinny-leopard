c67e77e993299454d2dbe7118e6c648698525494
James Bates <jimothybates@gmail.com>
2019-05-19 15:37:07 +0100

    added cloudinary and changed policy so that show page works, finally added navbar login logout function

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -7,10 +7,13 @@ class InstrumentsController < ApplicationController
 
   def new
     @instrument = Instrument.new
+    authorize @instrument
   end
 
   def create
     @instrument = Instrument.new(instrument_params)
+    @instrument.user = current_user
+    authorize @instrument
     if @instrument.save
       redirect_to instrument_path(@instrument)
     else
@@ -33,6 +36,7 @@ class InstrumentsController < ApplicationController
     end
     @instrument = Instrument.find(params[:id])
     @bookings = @instrument.bookings
+    authorize @instrument
     @alert_message = "This is #{@instrument.user.first_name}'s instrument"
   end
 
