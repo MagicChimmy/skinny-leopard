8d87ae6cfd23881166067ce1dac382177530b202
Jan Linhart <jan.b.linhart@gmail.com>
2019-05-21 17:39:12 +0100

    fixed search placeholder and error message

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
     if params[:search].nil? || params[:search] == ""
       @instruments = Instrument.all
     elsif Instrument.search_by_name_and_location_and_category(params[:search]).empty?
-      redirect_to(root_path, alert: "Empty!")
+      redirect_to(root_path, alert: "Nothing found, try again!")
     else
       @instruments = Instrument.search_by_name_and_location_and_category(params[:search])
     end
